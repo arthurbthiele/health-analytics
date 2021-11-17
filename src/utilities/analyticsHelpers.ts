35ae47509c3e173eeb57f96b55bc9af62c620cc8
arthur <arthurbthiele@gmail.com>
2021-11-17 20:01:09 +1100

    fix date display

--- src/utilities/analyticsHelpers.ts
+++ src/utilities/analyticsHelpers.ts
@@ -10,10 +10,17 @@ function getAnalytics(timeseries: Timeseries): StatisticalSummary {
   const count = timeseries.dataSet.length;
   if (count === 0) {
     return {
-      count: 0,
+      count,
     };
   }
+
   const average = mean(timeseries.dataSet.map((datum) => datum.y));
+  if (count === 1) {
+    return {
+      count,
+      average,
+    };
+  }
   const deviation = sampleStandardDeviation(
     timeseries.dataSet.map((datum) => datum.y)
   );
